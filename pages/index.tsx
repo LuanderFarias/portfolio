638b0beb1232867456983338acf4e51792e52f06
LuanderFarias <69652684+LuanderFarias@users.noreply.github.com>
2023-05-24 20:58:40 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ const Home: NextPage = () => {
           <div className="mt-64 place-items-center justify-center md:grid mx-4 mb-16">
               <div className="md:flex gap-x-20">
                 <div className="self-center mb-8">
-                    <h1 className="text-4xl font-semibold">Hey 👋 What's up?</h1>
-                    <p className="text-xl mt-4">My name is Luander, i'm a Brazilian software enginner 💻 powered by coffee ☕, code ⌨ and Linux 🐧 who likes to create open-source 🔓 solutions to problems i find. Always working and having ideas. My brain don't stop (pls send help);</p>
+                    <h1 className="text-4xl font-semibold">Hey, What's up?</h1>
+                    <p className="text-xl mt-4">My name is Luander, i'm a Brazilian software enginner powered by coffee, code and Linux who likes to create open-source solutions to problems i find. Always working and having ideas. My brain don't stop (pls send help);</p>
                 </div>
             </div>
         </div>
